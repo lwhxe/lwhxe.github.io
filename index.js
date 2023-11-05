8360ed0b99b6e99b91b7096406cbd64df176ff9a
lwhxe <149725738+lwhxe@users.noreply.github.com>
2023-11-05 20:34:44 +0100

    wer

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const logo = document.querySelector('.logoholder img.logoimage');
     const buttons = document.querySelectorAll('.button img'); // Select all button images
-
-    // Event listener for the logo
-    logo.addEventListener('mousemove', (e) => {
-        const rect = logo.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-        const distance = Math.sqrt(Math.pow(x - centerX, 2) + Math.pow(y - centerY, 2));
-        const maxDistance = Math.sqrt(Math.pow(centerX, 2) + Math.pow(centerY, 2));
-        // Adjust brightness based on cursor position
-        const brightness = 130 - (distance / maxDistance * 30);
-        logo.style.filter = `brightness(${brightness}%)`;
-    });
-
-    logo.addEventListener('mouseleave', () => {
-        logo.style.filter = 'brightness(100%)';
-    });
-
     // Add event listeners to each button for the brightness effect
     buttons.forEach((button) => {
         button.addEventListener('mousemove', (e) => {
