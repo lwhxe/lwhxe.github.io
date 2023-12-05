86ace7d69ad0cd58b0a8c312b8d7f09316fca060
lwhxe <149725738+lwhxe@users.noreply.github.com>
2023-12-05 20:25:15 +0100

    Added first subdir

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-var buttons = document.getElementsByClassName("image-button");
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("mouseenter", function() {
-        // Actions to perform when hovered
-        this.querySelector('.hover-content').style.display = 'block';
-    });
+function addHoverEffect(elements, hoverClassName) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("mouseenter", function() {
+            var hoverContent = this.querySelector('.' + hoverClassName);
+            if (hoverContent) {
+                hoverContent.style.display = 'block';
+            }
+        });
 
-    buttons[i].addEventListener("mouseleave", function() {
-        // Actions to perform when hover ends
-        this.querySelector('.hover-content').style.display = 'none';
-    });
+        elements[i].addEventListener("mouseleave", function() {
+            var hoverContent = this.querySelector('.' + hoverClassName);
+            if (hoverContent) {
+                hoverContent.style.display = 'none';
+            }
+        });
+    }
 }
+
+// Apply to image buttons
+var imageButtons = document.getElementsByClassName("image-button");
+addHoverEffect(imageButtons, 'hover-content');
+
+// Apply to downlinks
+var downlinks = document.getElementsByClassName("downlink");
+addHoverEffect(downlinks, 'downlink-hover');
