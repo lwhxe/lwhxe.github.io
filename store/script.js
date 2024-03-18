0cefb7c6908349e0ee67a26ea1b5d5939e0377aa
lwhxe <149725738+lwhxe@users.noreply.github.com>
2024-03-18 14:25:40 +0100

    Update script.js

--- store/script.js
+++ store/script.js
@@ -1,8 +1,6 @@
-<script>
-    document.addEventListener('mousemove', function(e) {
-        var sidebar = document.getElementById('sidebar');
-        if (e.pageX <= 5) { // Adjust if needed
-            sidebar.style.left = '0'; // Show sidebar
-        }
-    });
-</script>
+document.addEventListener('mousemove', function(e) {
+    var sidebar = document.getElementById('sidebar');
+    if (e.pageX <= 5) { // Adjust if needed
+        sidebar.style.left = '0'; // Show sidebar
+    }
+});
