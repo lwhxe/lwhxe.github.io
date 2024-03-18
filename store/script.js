5546b9e5e4554130841c242f5b86dc9458a6a930
lwhxe <149725738+lwhxe@users.noreply.github.com>
2024-03-18 14:38:58 +0100

    Update script.js

--- store/script.js
+++ store/script.js
@@ -1,6 +1,11 @@
 document.addEventListener('mousemove', function(e) {
     var sidebar = document.getElementById('sidebar');
-    if (e.pageX <= 5) { // Detects mouse near the left edge
-        sidebar.style.left = '0px'; // Shows the sidebar
+    // Check if sidebar is already closed to avoid reopening it.
+    if (e.pageX <= 5 && sidebar.style.left === "-200px") {
+        sidebar.style.left = '0px';
     }
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('sidebar').style.left = '-200px';
 });
