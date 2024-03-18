0b78d757f7470786a5041ba63c711536de44a811
lwhxe <149725738+lwhxe@users.noreply.github.com>
2024-03-18 14:46:40 +0100

    Update script.js

--- store/script.js
+++ store/script.js
@@ -1,13 +1,19 @@
 let isSidebarVisible = false; // Flag to track sidebar visibility
 
 document.addEventListener('mousemove', function(e) {
+    var sidebar = document.getElementById('sidebar');
+    var closeButton = document.getElementById('close-btn');
     if (e.pageX <= 5 && !isSidebarVisible) { // Only open sidebar if it's not already visible
-        document.getElementById('sidebar').style.left = '0px';
+        sidebar.style.left = '0px';
+        closeButton.style.display = 'flex'; // Show the X button
         isSidebarVisible = true; // Update flag
     }
 });
 
 document.getElementById('close-btn').addEventListener('click', function() {
-    document.getElementById('sidebar').style.left = '-200px';
+    var sidebar = document.getElementById('sidebar');
+    var closeButton = document.getElementById('close-btn');
+    sidebar.style.left = '-200px';
+    closeButton.style.display = 'none'; // Hide the X button
     isSidebarVisible = false; // Update flag
 });
