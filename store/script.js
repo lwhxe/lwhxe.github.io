133cc019ff839acdd2d0345224036172f35112ae
lwhxe <149725738+lwhxe@users.noreply.github.com>
2024-03-18 14:33:46 +0100

    Update script.js

--- store/script.js
+++ store/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('mousemove', function(e) {
     var sidebar = document.getElementById('sidebar');
-    if (e.pageX <= 5) { // Adjust if needed
-        sidebar.style.left = '0'; // Show sidebar
+    if (e.pageX <= 5) { // Detects mouse near the left edge
+        sidebar.style.left = '0px'; // Shows the sidebar
     }
 });
