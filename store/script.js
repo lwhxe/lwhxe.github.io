1534185c41e0c2eb992de3054ecdb45594cfa47b
lwhxe <149725738+lwhxe@users.noreply.github.com>
2024-03-18 14:42:43 +0100

    Update script.js

--- store/script.js
+++ store/script.js
@@ -1,11 +1,13 @@
+let isSidebarVisible = false; // Flag to track sidebar visibility
+
 document.addEventListener('mousemove', function(e) {
-    var sidebar = document.getElementById('sidebar');
-    // Check if sidebar is already closed to avoid reopening it.
-    if (e.pageX <= 5 && sidebar.style.left === "-200px") {
-        sidebar.style.left = '0px';
+    if (e.pageX <= 5 && !isSidebarVisible) { // Only open sidebar if it's not already visible
+        document.getElementById('sidebar').style.left = '0px';
+        isSidebarVisible = true; // Update flag
     }
 });
 
 document.getElementById('close-btn').addEventListener('click', function() {
     document.getElementById('sidebar').style.left = '-200px';
+    isSidebarVisible = false; // Update flag
 });
