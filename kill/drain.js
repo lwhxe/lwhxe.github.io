7733cfb0ed65c21ff18e5c1a9fd289ccacb7d317
lwhxe <149725738+lwhxe@users.noreply.github.com>
2024-04-15 17:35:13 +0200

    Update drain.js

--- kill/drain.js
+++ kill/drain.js
@@ -4,14 +4,13 @@ const resultElement = document.getElementById('result');
 
 let computationWorker = null;
 
-if (!computationWorker) {
-    computationWorker = new Worker('worker.js');
-    computationWorker.onmessage = function(event) {
-        console.log('Received from worker:', event.data);
-        resultElement.textContent = event.data;  // Update the UI with each received result
-    };
-    console.log('Computation started.');
-});
+
+computationWorker = new Worker('worker.js');
+computationWorker.onmessage = function(event) {
+    console.log('Received from worker:', event.data);
+    resultElement.textContent = event.data;  // Update the UI with each received result
+};
+console.log('Computation started.');
 
 stopButton.addEventListener('click', function() {
     if (computationWorker) {
